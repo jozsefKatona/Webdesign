758cd70365679ea46240a84010df4dc742cdcdca
jkwatercube <katonajk2004@gmail.com>
2024-04-05 20:33:11 +0200

    update

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ function draw() {
     const c = document.getElementById("myCanvas");
     const ctx = c.getContext("2d");
     const img = document.getElementById("dogPaw");
-    ctx.drawImage(img, 20, 0, 120, 120);
+
+    
+    ctx.drawImage(img, 110, 0, 120, 120);
+      
+    
   } 
   draw();
+  
